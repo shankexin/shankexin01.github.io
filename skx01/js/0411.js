63a682a436decbfea5997ec39fbcae6cb4d61c41
shankexin <38524549+shankexin@users.noreply.github.com>
2018-05-08 20:33:24 +0800

    Add files via upload

---
+++ skx01/js/0411.js
@@ -0,0 +1,9 @@
+function showPic(whichpic){
+
+  
+   // var source=whichpic.getAttribute("href");
+   // var placehoder=document.getElementById("placehoder");
+   // placehoder.setAttribute("src",source);
+placehoder=document.getElementById("placehoder").setAttribute("src",whichpic.getAttribute("href"));
+
+}
